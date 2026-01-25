5533d1307714b70ce8a04603a4a47ce6792bef94
LOGESH SANKAR <145207259+logeshssankar@users.noreply.github.com>
2026-01-25 21:13:57 +0530

    Add files via upload

---
+++ length convertor/script.js
@@ -0,0 +1,8 @@
+// let name = prompt('what is your name?')
+// console.log('Hi! ', name)
+
+// let tickets = prompt('How many tickets do you want?')
+// console.log('You should pay ',tickets*20)//store value in string
+
+// let a = prompt('Enter a number')//store as a string
+// console.log(Number(a)+10)
